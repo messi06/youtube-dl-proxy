3795f98b303fb5d4ca336890a1bc04059c305de4
zfdev <jasonzhangod@gmail.com>
2019-06-15 01:27:18 +0800

    Update youtube-dl-server.py

--- youtube-dl-server.py
+++ youtube-dl-server.py
@@ -1,52 +1,121 @@
+from __future__ import unicode_literals
 import json
 import os
 import subprocess
 from queue import Queue
 from bottle import route, run, Bottle, request, static_file
 from threading import Thread
+import youtube_dl
+from pathlib import Path
+from collections import ChainMap
 
 app = Bottle()
+
+
+app_defaults = {
+    'YDL_FORMAT': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]',
+    'YDL_EXTRACT_AUDIO_FORMAT': None,
+    'YDL_EXTRACT_AUDIO_QUALITY': '192',
+    'YDL_RECODE_VIDEO_FORMAT': None,
+    'YDL_OUTPUT_TEMPLATE': '/youtube-dl/%(title)s [%(id)s].%(ext)s',
+    'YDL_ARCHIVE_FILE': None,
+    'YDL_SERVER_HOST': '0.0.0.0',
+    'YDL_SERVER_PORT': 8080,
+}
+
 
 @app.route('/youtube-dl')
 def dl_queue_list():
     return static_file('index.html', root='./')
 
+
 @app.route('/youtube-dl/static/:filename#.*#')
 def server_static(filename):
     return static_file(filename, root='./static')
 
+
 @app.route('/youtube-dl/q', method='GET')
 def q_size():
-    return { "success" : True, "size" : json.dumps(list(dl_q.queue)) }
+    return {"success": True, "size": json.dumps(list(dl_q.queue))}
+
 
 @app.route('/youtube-dl/q', method='POST')
 def q_put():
-    url = request.forms.get( "url" )
-    if "" != url:
-        dl_q.put( url )
-        print("Added url " + url + " to the download queue")
-        return { "success" : True, "url" : url }
-    else:
-        return { "success" : False, "error" : "dl called without a url" }
+    url = request.forms.get("url")
+    options = {
+        'format': request.forms.get("format")
+    }
+
+    if not url:
+        return {"success": False, "error": "/q called without a 'url' query param"}
+
+    dl_q.put((url, options))
+    print("Added url " + url + " to the download queue")
+    return {"success": True, "url": url, "options": options}
+
 
 def dl_worker():
     while not done:
-        item = dl_q.get()
-        download(item)
+        url, options = dl_q.get()
+        download(url, options)
         dl_q.task_done()
 
-def download(url):
-    print("Starting download of " + url)
-    subprocess.run(["youtube-dl", "-o", "/youtube-dl/.incomplete/%(title)s.%(ext)s", "-f", "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]", "--proxy", "socks5://192.168.1.12:10808", "--exec", "touch {} && mv {} /youtube-dl/", "--merge-output-format", "mp4", "--download-archive", "/youtube-dl/.incomplete/downloaded.txt", "--no-check-certificate", "-i", url])
-    print("Finished downloading " + url)
 
-dl_q = Queue();
-done = False;
+def get_ydl_options(request_options):
+    request_vars = {
+        'YDL_EXTRACT_AUDIO_FORMAT': None,
+        'YDL_RECODE_VIDEO_FORMAT': None,
+    }
+
+    requested_format = request_options.get('format', 'bestvideo')
+
+    if requested_format in ['aac', 'flac', 'mp3', 'm4a', 'opus', 'vorbis', 'wav']:
+        request_vars['YDL_EXTRACT_AUDIO_FORMAT'] = requested_format
+    elif requested_format == 'bestaudio':
+        request_vars['YDL_EXTRACT_AUDIO_FORMAT'] = 'best'
+    elif requested_format in ['mp4', 'flv', 'webm', 'ogg', 'mkv', 'avi']:
+        request_vars['YDL_RECODE_VIDEO_FORMAT'] = requested_format
+
+    ydl_vars = ChainMap(request_vars, os.environ, app_defaults)
+
+    postprocessors = []
+
+    if(ydl_vars['YDL_EXTRACT_AUDIO_FORMAT']):
+        postprocessors.append({
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': ydl_vars['YDL_EXTRACT_AUDIO_FORMAT'],
+            'preferredquality': ydl_vars['YDL_EXTRACT_AUDIO_QUALITY'],
+        })
+
+    if(ydl_vars['YDL_RECODE_VIDEO_FORMAT']):
+        postprocessors.append({
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': ydl_vars['YDL_RECODE_VIDEO_FORMAT'],
+        })
+
+    return {
+        'format': ydl_vars['YDL_FORMAT'],
+        'postprocessors': postprocessors,
+        'outtmpl': ydl_vars['YDL_OUTPUT_TEMPLATE'],
+        'download_archive': ydl_vars['YDL_ARCHIVE_FILE'],
+        'proxy': 'socks5://192.168.1.12:10808'
+    }
+
+
+def download(url, request_options):
+    with youtube_dl.YoutubeDL(get_ydl_options(request_options)) as ydl:
+        ydl.download([url])
+
+
+dl_q = Queue()
+done = False
 dl_thread = Thread(target=dl_worker)
 dl_thread.start()
 
 print("Started download thread")
 
-app.run(host='0.0.0.0', port=8765, debug=True)
+app_vars = ChainMap(os.environ, app_defaults)
+
+app.run(host=app_vars['YDL_SERVER_HOST'], port=app_vars['YDL_SERVER_PORT'], debug=True)
 done = True
 dl_thread.join()
